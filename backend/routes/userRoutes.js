1f2a793e59385687ecfddea532f82b1974dfbd33
dikkat-lutfen <dikkatlutfen5@gmail.com>
2023-02-20 00:00:58 +0200

    backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 const router = express.Router()
-const {getUsers, registerUser, loginUser} = require ("../controllers/userController.js")
+const{verifyIsLoggedIn,verifyIsAdmin}=require("../middleware/verifyAuthToken")
+const {getUsers, registerUser, loginUser,updateUserProfile} = require ("../controllers/userController.js")
 /* const { route } = require("./categoryRoutes.js") */
 
 
@@ -11,7 +12,10 @@ router.post("/login", loginUser)
 
 // user logged in routes
 
+router.use(verifyIsLoggedIn)
+router.put("/profile",updateUserProfile)
 //admin routes
+router.use(verifyIsAdmin)
 router.get("/", getUsers)
 
 module.exports = router;
