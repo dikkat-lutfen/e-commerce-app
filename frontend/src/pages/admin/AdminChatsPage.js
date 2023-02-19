ce125d419bd121d28a20fc728d258a1ea039f964
dikkat-lutfen <dikkatlutfen5@gmail.com>
2023-02-19 23:23:16 +0200

    frontend

--- frontend/src/pages/admin/AdminChatsPage.js
+++ frontend/src/pages/admin/AdminChatsPage.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import AdminChatRoomComponent from '../../components/admin/AdminChatRoomComponent'
+import AdminLinksComponent from '../../components/admin/AdminLinksComponent'
+import {Row,Col} from "react-bootstrap"
 
 function AdminChatsPage() {
   return (
-    <div>
-      <h1>This is AdminChatsPage</h1>
-    </div>
+    <Row className="m-5">
+<Col md={2}>
+    <AdminLinksComponent/>
+</Col>
+<Col md={10}>
+  <Row style={{marginBottom:"100px"}}>
+    <AdminChatRoomComponent/>
+  </Row>
+</Col>
+    </Row>
   )
 }
 
