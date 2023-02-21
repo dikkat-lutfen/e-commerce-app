155f93f61ddfc5b49a02a2aad7825d8534e977cf
dikkat-lutfen <dikkatlutfen5@gmail.com>
2023-02-21 15:11:34 +0200

    connection

--- frontend/src/pages/admin/AdminUsersPage.js
+++ frontend/src/pages/admin/AdminUsersPage.js
@@ -1,9 +1,20 @@
 import UsersPageComponent from "./components/UsersPageComponent"
+import axios from "axios"
 
+
+const fetchUsers = async (abctrl) => {
+  const {data} = await axios.get("/api/users", {
+      signal: abctrl.signal,
+  });
+  return data
+}
+
+const deleteUser = async (userId)=>{
+  const {data}= await axios.delete(`/api/users/${userId}`)
+}
 function AdminUsersPage() {
 
- return <UsersPageComponent/>
-
+ return <UsersPageComponent fetchUsers={fetchUsers} deleteUser={deleteUser}/>
  
 }
 
